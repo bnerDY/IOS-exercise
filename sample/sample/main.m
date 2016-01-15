c1b51787bab6f029ad02222303c9a11f742ccb7a
martin <dingxin.yu@uqconnect.edu.au>
2016-01-15 03:16:38 +0000

    update exercise command line project.

---
+++ sample/sample/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  sample
+//
+//  Created by DINGXIN YU on 15/01/2016.
+//  Copyright © 2016 DINGXIN YU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    int A = 1;
+    int B = 0;
+    if (A==B) {
+        printf("A equals B\n");
+    } else {
+        printf("A does not equal B\n");
+    }
+    return 0;
+}
